9310819b605b6d874dc70fb9cbfd6780abb960f6
Рафиков Айдар Робертович <190210@edu.psuti.ru>
2021-08-25 23:46:58 +0400

    bugfix with error Serializing

--- src/main/kotlin/data/Number.kt
+++ src/main/kotlin/data/Number.kt
@@ -1,7 +1,9 @@
 package data
 
+import java.io.Serializable
+
 /**
  *
  * @author Aydar Rafikov
  */
-data class Number(val number: String, val id: Long)
+data class Number(val number: String, val id: Long) : Serializable
